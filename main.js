81d913d3bc1d979954df4051751d1f82b3f1609c
HARSHIT BARGUJAR <42277796+harshit2608@users.noreply.github.com>
2019-03-09 12:34:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 //javascipt in here
+
+
+//SCROLL BAR SCRIPT
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
